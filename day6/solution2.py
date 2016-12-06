b2b99b8636bc9f1821f7d204a5d14c04ca678d61
Tom Swartz <tom.swartz07@gmail.com>
2016-12-06 16:50:31 -0500

    Day 6
    Easy peasy
    
    There's almost no difference between Part 1 and Part 2.

---
+++ day6/solution2.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python
+
+'''
+Advent of Code
+Day 6, Part 2
+'''
+
+from collections import Counter
+
+with open('test.txt') as fd:
+    TEST = fd.read().strip()
+
+with open('input.txt') as f:
+    DATA = f.read().strip()
+
+def count_data(counter_data):
+    '''
+    Function to calculate most common char in input
+    '''
+    data_counted = [Counter(x).most_common() for x in zip(*counter_data.split('\n'))]
+    message = ''.join(x[-1][0] for x in data_counted)
+    return message
+
+print('Validation: {}\nMessage: {}'.format(count_data(TEST), count_data(DATA)))
