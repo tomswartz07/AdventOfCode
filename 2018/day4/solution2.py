c693dc428e384ecc29f4a3c42d5ca2e0b8d75f26
Tom Swartz <tom.swartz07@gmail.com>
2018-12-04 14:14:50 -0500

    Day 4 Solutions

---
+++ 2018/day4/solution2.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python
+
+from collections import Counter, defaultdict
+from datetime import datetime
+
+with open('input.txt', 'r') as f:
+    data = f.read().rstrip('\n').splitlines()
+
+guards = defaultdict(Counter)
+for t, m in [l.split('] ') for l in sorted(data) if l]:
+    t = datetime.strptime(t, '[%Y-%m-%d %H:%M')
+    print(t)
+    if 'Guard' in m:
+        g = int(m.split('#')[1].split()[0])
+        print(g)
+    if 'asleep' in m:
+        start = t
+    if 'wakes' in m:
+        minutes = int((t - start).total_seconds() // 60)
+        guards[g].update(Counter((start.minute+i)%60 for i in range(minutes)))
+
+(_, minute), guard_id = max((c.most_common()[0][::-1], guard_id) for guard_id, c in guards.items())
+print(guard_id * minute)
