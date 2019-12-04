fb884d26bb3b093bcc84433b6d93f2883827c593
Tom Swartz <tom.swartz07@gmail.com>
2019-12-04 14:28:02 -0500

    Day 3

---
+++ 2019/day3/solution.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python
+from collections import defaultdict
+
+with open('input.txt', 'r') as f:
+    wires = f.read().splitlines()
+
+points = defaultdict(list)
+i = 0
+
+for l in wires:
+    movements = l.split(",")
+    x = 0
+    y = 0
+
+    for m in movements:
+        unit = int(m[1:])
+        if   "R" in m:
+            for u in range(1, unit+1):
+                x += 1
+                points[i].append((x, y))
+
+        elif "L" in m:
+            for u in range(1, unit+1):
+                x -= 1
+                points[i].append((x, y))
+
+
+        elif "U" in m:
+            for u in range(1, unit+1):
+                y += 1
+                points[i].append((x, y))
+
+
+        elif "D" in m:
+            for u in range(1, unit+1):
+                y -= 1
+                points[i].append((x, y))
+    i = i + 1
+
+result = set(points[0]) & set(points[1])
+minx = min([abs(point[0]) + abs(point[1]) for point in result])
+print("Result is:", minx)
