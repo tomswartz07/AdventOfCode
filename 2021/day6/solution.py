6841910b55d50147d6ed0f3cb4c96537c83313fb
Tom Swartz <tom.swartz07@gmail.com>
2021-12-06 14:35:00 -0500

    Day 6 2021

---
+++ 2021/day6/solution.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python
+
+with open('input.txt', 'r', encoding="utf-8") as f:
+    data = f.read().split(",")
+    data = [int(line) for line in data]
+
+def population(newIn, days):
+    n = [newIn.count(i) for i in range(9)]
+    for _ in range(days):
+        z = n[0]
+        n[0:8] = n[1:9]
+        n[6] += z
+        n[8] = z
+    return sum(n)
+
+print(population(data,80))
